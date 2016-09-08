37db52aa4d620120083d8b9b25672d0550c238b9
Walkyso Júnior <walkyso.junior@ifrn.edu.br>
2016-09-08 13:31:21 -0300

    ajuste

--- newadmin/utils.py
+++ newadmin/utils.py
@@ -19,7 +19,7 @@ from django.forms.utils import flatatt
 from django.http import HttpResponse
 from django.template.defaultfilters import floatformat
 from django.template.loader import render_to_string
-from django.utils.encoding import force_unicode
+from django.utils.encoding import force_text
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from localflavor.br.forms import BRCPFField
@@ -32,7 +32,7 @@ phone_digits_re = re.compile(r'^(\d{2})[-\.]?(\d{4,5})[-\.]?(\d{4})$')
 
 def decimal_to_money(value, precision=2):
     value = floatformat(value, precision)
-    value, decimal = force_unicode(value).split('.')
+    value, decimal = force_text (value).split('.')
     value = intcomma(value)
     value = value.replace(',', '.') + ',' + decimal
     return value
