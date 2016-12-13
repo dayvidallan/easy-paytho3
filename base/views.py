466b55c8f38892b000b69b1fe83727fd43ca16e0
Walkyso Júnior <walkyso.junior@ifrn.edu.br>
2016-12-13 09:23:33 -0200

    ajuste

--- base/views.py
+++ base/views.py
@@ -425,6 +425,8 @@ def itens_solicitacao(request, solicitacao_id):
         ultima_movimentacao = MovimentoSolicitacao.objects.filter(solicitacao=solicitacao).latest('id')
         if ultima_movimentacao.setor_destino == setor_do_usuario and ultima_movimentacao.data_recebimento:
             recebida_no_setor = True
+    elif setor_atual == setor_do_usuario:
+        recebida_no_setor = True
     title=u'Solicitação - Memorando: %s' % solicitacao
     itens = ItemSolicitacaoLicitacao.objects.filter(solicitacao=solicitacao).order_by('item')
     ja_registrou_preco = ItemQuantidadeSecretaria.objects.filter(solicitacao=solicitacao, secretaria=request.user.pessoafisica.setor.secretaria, aprovado=True)
