5b98e9eb7abe8216a1af721fe48b4a9aa6bce876
Admin <walkyso@gmail.com>
2017-01-23 12:37:38 +0000

    ajus

---
+++ sql/backup_media.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose = Backup of Important Data
+#Created on 17-1-2012
+#Author = Hafiz Haider
+#Version 1.0
+#START
+TIME=`date +%b-%d-%y`            # This Command will add date in Backup File Name.
+FILENAME=backup_media-$TIME.tar.gz    # Here i define Backup file name format.
+SRCDIR=/home/workspace/licita/media/                    # Location of Important Data Directory (Source of backup).
+DESDIR=/home/licita_bkp/            # Destination of backup file.
+tar -cpzf $DESDIR/$FILENAME $SRCDIR
+#END
