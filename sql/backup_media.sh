cd03309fe9bde852ab224d74a46660c24beca20a
Admin <walkyso@gmail.com>
2017-01-23 16:29:15 +0000

    a

--- sql/backup_media.sh
+++ sql/backup_media.sh
@@ -7,7 +7,7 @@
 TIME=`date +%b-%d-%y`            # This Command will add date in Backup File Name.
 FILENAME=backup_media-$TIME.tar.gz    # Here i define Backup file name format.
 SRCDIR=/home/workspace/licita/media/                    # Location of Important Data Directory (Source of backup).
-DESDIR=/home/licita_bkp/            # Destination of backup file.
+DESDIR=~/Dropbox/licita_bkp/            # Destination of backup file.
 tar -cpzf $DESDIR/$FILENAME $SRCDIR
-pg_dump --dbname=postgresql://walkyso:licitaeasy@127.0.0.1:5432/licita | gzip > /home/licita_bkp/$(date +%Y-%m-%d).psql.gz
+pg_dump --dbname=postgresql://walkyso:licitaeasy@127.0.0.1:5432/licita | gzip > ~/Dropbox/licita_bkp/$(date +%Y-%m-%d).psql.gz
 #END
