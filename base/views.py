04ff16a55750bc3f06cb62f507cfceea16012add
Walkyso <walkyso@gmail.com>
2017-08-07 08:31:13 -0300

    a

--- base/views.py
+++ base/views.py
@@ -6762,7 +6762,7 @@ def anexo_38(request, pregao_id):
     contador_total = 0
     for idx, item in enumerate(itens, 0):
 
-        resultado = ResultadoItemPregao.objects.filter(item=item, participante__excluido_dos_itens=False, participante__desclassificado=False, item__solicitacao=pregao.solicitacao, item__situacao__in=[ItemSolicitacaoLicitacao.CADASTRADO, ItemSolicitacaoLicitacao.CONCLUIDO]).order_by('ordem')
+        resultado = ResultadoItemPregao.objects.filter(item=item, situacao=ResultadoItemPregao.CLASSIFICADO, participante__excluido_dos_itens=False, participante__desclassificado=False, item__solicitacao=pregao.solicitacao, item__situacao__in=[ItemSolicitacaoLicitacao.CADASTRADO, ItemSolicitacaoLicitacao.CONCLUIDO]).order_by('ordem')
         contador = 1
         for result in resultado:
             row_index = contador_total + 1
@@ -6772,14 +6772,9 @@ def anexo_38(request, pregao_id):
             w_sheet.write(row_index, 1, item.material.nome[:100])
             w_sheet.write(row_index, 2, contador)
             w_sheet.write(row_index, 3, format_money(result.valor))
-            if result.participante.nome_representante:
-                w_sheet.write(row_index, 4, result.participante.nome_representante)
-                w_sheet.write(row_index, 5, u'CPF')
-                w_sheet.write(row_index, 6, result.participante.cpf_representante)
-            else:
-                w_sheet.write(row_index, 4, result.participante.fornecedor.razao_social)
-                w_sheet.write(row_index, 5, u'CNPJ')
-                w_sheet.write(row_index, 6, result.participante.fornecedor.cnpj)
+            w_sheet.write(row_index, 4, result.participante.fornecedor.razao_social)
+            w_sheet.write(row_index, 5, u'CNPJ')
+            w_sheet.write(row_index, 6, str(result.participante.fornecedor.cnpj).replace('.', '').replace('-', '').replace('/', ''))
 
 
             contador += 1
