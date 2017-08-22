09dc6e52af86325af343ed7252dacde2eb0fc212
Walkyso <walkyso@gmail.com>
2017-08-22 11:27:36 -0300

    ajuste no codigo de atualizacao

--- base/management/commands/atualiza_valor_pedidos.py
+++ base/management/commands/atualiza_valor_pedidos.py
@@ -3,7 +3,7 @@
 from django.core.management.base import BaseCommand
 
 
-from base.models import PedidoCredenciamento, PedidoAtaRegistroPreco, PedidoContrato
+from base.models import PedidoCredenciamento, PedidoAtaRegistroPreco, PedidoContrato, Pregao
 
 
 class Command(BaseCommand):
@@ -20,3 +20,12 @@ class Command(BaseCommand):
         for pedido in PedidoContrato.objects.all():
             pedido.valor = pedido.item.valor
             pedido.save()
+
+
+        for pregao in Pregao.objects.all():
+            if pregao.situacao == u'Cadastrado':
+                if pregao.data_homologacao:
+                    pregao.situacao = u'Homologado'
+                else:
+                    pregao.situacao = u'Publicado'
+                pregao.save()
