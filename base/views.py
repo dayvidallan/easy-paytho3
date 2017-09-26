d6bc08a466de64ea6e98da1fe200d32bd617ea17
Walkyso <walkyso@gmail.com>
2017-09-26 15:26:26 -0300

    a

--- base/views.py
+++ base/views.py
@@ -5135,6 +5135,11 @@ def gerar_ordem_compra(request, solicitacao_id):
         messages.error(request, u'Esta solicitação possui mais de uma proposta empatada com o melhor valor.')
         return HttpResponseRedirect(u'/base/itens_solicitacao/%s/' % solicitacao_id)
 
+    if OrdemCompra.objects.filter(solicitacao=solicitacao).exists():
+        messages.error(request, u'Esta solicitação já possui uma ordem de compra.')
+        return HttpResponseRedirect(u'/base/itens_solicitacao/%s/' % solicitacao_id)
+        
+
     id_sessao = "%s_solicitacao" % (request.user.pessoafisica.id)
     request.session[id_sessao] = solicitacao.id
     title = u'Gerar Ordem de Compra/Serviço - %s' % solicitacao
