ae5254aa08e3bee6ec3c3d91b5522377f853870f
Walkyso <walkyso.junior@ifrn.edu.br>
2018-06-08 13:28:42 -0300

    a

--- base/views.py
+++ base/views.py
@@ -6148,22 +6148,22 @@ def memorando(request, solicitacao_id):
     unidades  = []
     descricoes = []
 
-    for item in solicitacao.itemsolicitacaolicitacao_set.all():
-        itens.append(item)
-        quantidades.append(item.quantidade)
-        unidades.append(item.unidade)
-        descricoes.append(item.material.nome)
+    # for item in solicitacao.itemsolicitacaolicitacao_set.all():
+    #     itens.append(item)
+    #     quantidades.append(item.quantidade)
+    #     unidades.append(item.unidade)
+    #     descricoes.append(item.material.nome)
 
     dicionario = {
         '#NUM#' : solicitacao.num_memorando,
         '#MUNICIPIO#' : municipio or u'-',
         '#DATA#': datetime.date.today(),
-        '#OBJETIVO#': solicitacao.objetivo,
-        '#OBJETO#': solicitacao.objeto,
-        '#IT#': libreoffice_new_line(itens or '-'),
-        '#QUANT#': libreoffice_new_line(quantidades or '-'),
-        '#UN#': libreoffice_new_line(unidades or '-'),
-        '#DES#': libreoffice_new_line(descricoes or '-'),
+        # '#OBJETIVO#': solicitacao.objetivo,
+        # '#OBJETO#': solicitacao.objeto,
+        # '#IT#': libreoffice_new_line(itens or '-'),
+        # '#QUANT#': libreoffice_new_line(quantidades or '-'),
+        # '#UN#': libreoffice_new_line(unidades or '-'),
+        # '#DES#': libreoffice_new_line(descricoes or '-'),
 
     }
     template_docx = zipfile.ZipFile(os.path.join(settings.MEDIA_ROOT, 'upload/modelos/memorando.docx'))
