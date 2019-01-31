75d6cf993b2936c15d7b145e7a97f896453a8262
Walkyso <walkyso.junior@ifrn.edu.br>
2019-01-31 13:26:11 -0300

    a

--- base/views.py
+++ base/views.py
@@ -815,7 +815,8 @@ def ver_pregoes(request):
 
         if form.cleaned_data.get('secretaria'):
             pregoes = pregoes.filter(solicitacao__setor_origem__secretaria=form.cleaned_data.get('secretaria'))
-
+    if eh_ordenador_despesa:
+        pregoes = pregoes.order_by('data_homologacao')
     return render(request, 'ver_pregoes.html', locals(), RequestContext(request))
 
 @login_required()
