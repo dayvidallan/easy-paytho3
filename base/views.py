46c6fa29ce1e6ce4aba1bd5f00b1128ad56ff54b
Walkyso <walkyso.junior@ifrn.edu.br>
2019-02-27 13:50:28 -0300

    a

--- base/views.py
+++ base/views.py
@@ -11766,7 +11766,8 @@ def baixar_contratos_portal(request):
             w_sheet.write(row_index, 6, format_money(item.get_saldo_disponivel()))
             data = u'%s a %s ' % (item.data_inicio.strftime('%d/%m/%Y'), item.data_fim.strftime('%d/%m/%Y'))
             w_sheet.write(row_index, 7, data)
-            w_sheet.write(row_index, 8, item.get_fornecedor())
+            fornecedor = u'%s (%s)' % (item.get_fornecedor().razao_social, item.get_fornecedor().cnpj)
+            w_sheet.write(row_index, 8, fornecedor)
             contador += 1
             conta_item += 1
 
