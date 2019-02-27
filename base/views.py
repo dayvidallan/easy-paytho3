a1ae93ff6201fc51dd2daeab13d6e3caa43f63f5
Walkyso <walkyso.junior@ifrn.edu.br>
2019-02-27 15:09:21 -0300

    A

--- base/views.py
+++ base/views.py
@@ -896,11 +896,9 @@ def baixar_editais(request):
         if form.cleaned_data.get('numero'):
             pregoes = pregoes.filter(Q(num_pregao__icontains=form.cleaned_data.get('numero')) | Q(objeto__icontains=form.cleaned_data.get('numero')))
 
-        if form.cleaned_data.get('data_inicial'):
-            pregoes = pregoes.filter(data_abertura__gte=form.cleaned_data.get('data_inicial'))
+        if form.cleaned_data.get('ano'):
+            pregoes = pregoes.filter(data_abertura__year=form.cleaned_data.get('ano'))
 
-        if form.cleaned_data.get('data_final'):
-            pregoes = pregoes.filter(data_abertura__lte=form.cleaned_data.get('data_final'))
 
         if form.cleaned_data.get('situacao'):
             if form.cleaned_data.get('situacao') == Pregao.ADJUDICADO:
