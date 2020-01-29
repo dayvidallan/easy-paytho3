c6b7e44eecdb880901dbb5b16eec27b0c6b447ae
Walkyso Junior <walkyso.junior@ifrn.edu.br>
2020-01-29 10:07:08 -0300

    settings

--- licita/settings.py
+++ licita/settings.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
     )
 }
 
-SITE_URL = DEBUG and 'http://localhost:8000' or 'http://guamareserver.easygestaopublica.com.br'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -180,9 +180,24 @@ DATABASES = {
     }
 }
 
-try:
-    from settings_base import *
-except ImportError as e:
-    pass
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'senha' #my gmail password
+EMAIL_HOST_USER = 'email@gmail.com' #my gmail username
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
+#dados do zoho, nosso servidor de email
+#SMTP Host: smtp.zoho.com
+#SMTP Port: 465
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    ADMINS = (
+        ('Walkyso', 'walkyso@gmail.com'),
+    )
+
+SITE_URL = DEBUG and 'http://localhost:8000' or 'http://guamareserver.easygestaopublica.com.br'
