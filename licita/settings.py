194a7bdc266111956d4daa65305b189f5aa3ae4a
Walkyso Junior <walkyso.junior@ifrn.edu.br>
2020-02-07 08:27:56 -0300

    ajuste

--- licita/settings.py
+++ licita/settings.py
@@ -61,17 +61,6 @@ ROOT_URLCONF = 'licita.urls'
 
 WSGI_APPLICATION = 'licita.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'licita',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-    }
-}
-
 
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Recife'
@@ -169,35 +158,4 @@ REST_FRAMEWORK = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'licita',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-    }
-}
-
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'senha' #my gmail password
-EMAIL_HOST_USER = 'email@gmail.com' #my gmail username
-EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-
-#dados do zoho, nosso servidor de email
-#SMTP Host: smtp.zoho.com
-#SMTP Port: 465
-
-if DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
-    ADMINS = (
-        ('Walkyso', 'walkyso@gmail.com'),
-    )
-
-SITE_URL = DEBUG and 'http://localhost:8000' or 'http://guamareserver.easygestaopublica.com.br'
+from .local_settings import *
